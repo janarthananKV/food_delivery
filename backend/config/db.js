bf68ade43a1fe1adbf3fcc3445795bb4dfa94c1c
janarthananKV <jkm06032005@gmail.com>
2025-05-01 17:19:38 +0530

    update DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://Janarthanan:Krishna123@cluster0.9oeas.mongodb.net/food_delivery').then(()=>console.log("DB Connected"));
+    await mongoose.connect(`${process.env.MONGODB_URI}/food_delivery`).then(()=>console.log("DB Connected"));
     
 }
